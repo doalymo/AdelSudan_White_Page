4da50307537370d66d3f1ffc964d81f9ad98f4fa
doalymo <doalymo55@gmail.com>
2018-05-08 03:48:57 -0700

    h

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://localhost/person_db');
+var connection = mongoose.connect('mongodb://<doalymo>:<Doalymo55>@ds016118.mlab.com:16118/person_db');
  
 module.exports = connection;
